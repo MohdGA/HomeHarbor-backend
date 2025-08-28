0d1b01ecf65cd58ef674a898a1a5c9b572131675
sayed-ali-ga <alsayed3li2005@gmail.com>
2025-08-28 16:02:56 +0300

    Add Property models and serializers

---
+++ serializers/property.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel, Field
+from typing import Optional, List
+from .review import ReviewSchema
+from .user import UserResponseSchema
+
+
+
+class PropertySchema(BaseModel):
+    id: Optional[int] = Field(default=None)
+    title: str
+    price: int
+    numOfRooms: int
+    numOfBathrooms: int
+    location: str
+
+
+    reviews: List[ReviewSchema] = []
+    user: UserResponseSchema
+
+    class Config:
+        orm_mode = True
+
+
+
+
+    class PropertyCreateSchema(BaseModel):
+        title: str
+        price: int
+        numOfRooms: int
+        numOfBathrooms: int
+        location: str 
+
+
+
