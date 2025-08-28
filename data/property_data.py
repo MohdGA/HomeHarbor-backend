9747b63ea4ed7c0409ff3ce386f0d75e015817e9
mohd <generalassemblyse@gmail.com>
2025-08-28 17:21:17 +0300

    edit

---
+++ data/property_data.py
@@ -0,0 +1,18 @@
+from models.property import PropertyModel
+from models.review import ReviewModel
+from models.request import RequestModel
+
+property_list = [
+    PropertyModel(title="Apartment", price= 40, numberuser_id=1),
+    
+]
+
+
+review_list = [
+    
+]
+
+
+request_list = [
+    
+]
