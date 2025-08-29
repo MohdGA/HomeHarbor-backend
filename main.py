6c6ddd4ebe89d50f1df149d71f88f8394d3065f6
xabdulla <abdullashaker002@gmail.com>
2025-08-29 17:23:20 +0300

    added the review the main.py

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from fastapi import FastAPI
+from controllers.reviews import router as ReviewsRouter
 
 
 app = FastAPI()
 
+app.include_router(ReviewsRouter, prefix='/api')
 
 
 @app.get('/')
