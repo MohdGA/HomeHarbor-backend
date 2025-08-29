ce202e951e458f7ca8cfb40ae82bae61997ec9e2
sayed-ali-ga <alsayed3li2005@gmail.com>
2025-08-29 18:04:05 +0300

    update main.py

--- main.py
+++ main.py
@@ -5,9 +5,25 @@ from fastapi import FastAPI
 from controllers.users import router as UsersRouter
 from controllers.property import router as PropertyRouter
 from controllers.reviews import router as ReviewRouter
-
+from dotenv import load_dotenv
+load_dotenv()
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+origins = [
+    "http://127.0.0.1:5173",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins, 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(UsersRouter, prefix='/api')
 app.include_router(PropertyRouter, prefix='/api')
