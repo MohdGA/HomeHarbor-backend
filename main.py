75c5c2d04b87f2abfe4685db7db18858b1842a2f
MohdGA <generalassemblyse@gmail.com>
2025-09-07 13:04:05 +0300

    fix: add correct frontend origin with https

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ origins = [
     "http://localhost:5174",
     "http://127.0.0.1:5173",
     "http://localhost:5173",
-    "https://home-harbor-frontend-hn1tof513-homeharbors-projects.vercel.app/"
+    "https://home-harbor-frontend-hn1tof513-homeharbors-projects.vercel.app"
 ]
 
 app.add_middleware(
