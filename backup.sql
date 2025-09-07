520f9c85104c6824997b048b82292a8ae1052f2c
MohdGA <generalassemblyse@gmail.com>
2025-09-07 16:24:09 +0300

    Add database backup

---
+++ backup.sql
@@ -0,0 +1,26 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict kvfHdE86mGcmaTutJsCXt2VY77Ht76xye7jBRoLmLj3s6WWkw1axPAezVzq5O6H
+
+-- Dumped from database version 16.10 (Ubuntu 16.10-1.pgdg24.04+1)
+-- Dumped by pg_dump version 16.10 (Ubuntu 16.10-1.pgdg24.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict kvfHdE86mGcmaTutJsCXt2VY77Ht76xye7jBRoLmLj3s6WWkw1axPAezVzq5O6H
+
